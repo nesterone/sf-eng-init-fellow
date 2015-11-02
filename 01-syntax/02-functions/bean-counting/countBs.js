ed13413afe6b93d898fc0ba5261d1821f981eff0
nesterone <igornester@gmail.com>
2015-11-02 16:59:05 +0200

    -split bean counting in two exercises

--- 01-syntax/02-functions/bean-counting/countBs.js
+++ 01-syntax/02-functions/bean-counting/countBs.js
@@ -5,7 +5,6 @@
 
 // Your code here.
 
+
 console.log(countBs("BBC"));
 // → 2
-console.log(countChar("kakkerlak", "k"));
-// → 4
---
+++ 01-syntax/02-functions/bean-counting/countChar.js
@@ -0,0 +1,10 @@
+/**
+ *  [Bean Counting](http://eloquentjavascript.net/03_functions.html#h_XTmO7z7MPq)
+ *
+ */
+
+// Your code here.
+
+
+console.log(countChar("kakkerlak", "k"));
+// → 4
