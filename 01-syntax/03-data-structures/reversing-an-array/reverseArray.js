92851456677e046da0ad6a65703bcfa78f9c2179
yzhbankov <yzhbankov2@gmail.com>
2015-12-24 10:53:49 +0200

    Update reverseArray.js

--- 01-syntax/03-data-structures/reversing-an-array/reverseArray.js
+++ 01-syntax/03-data-structures/reversing-an-array/reverseArray.js
@@ -3,7 +3,22 @@
  *
  */
 
-// Your code here.
+function reverseArray(c){
+  var n = [], k = 0;
+  for (var i = c.length - 1; i >= 0; i = i - 1){
+    n[k] = c[i];
+    k = k + 1;}
+  return n;}
+
+var arrayValue = [1, 2, 3, 4, 5];
+function reverseArrayInPlace(x){
+var n = x.length - 1;
+for (var i = 0; i <= Math.floor((x.length - 1) / 2); i = i + 1){
+ r = x[i];
+ x[i] = x[n];
+ x[n] = r;
+ n = n - 1;}
+return x;}
 
 console.log(reverseArray(["A", "B", "C"]));
 // → ["C", "B", "A"];
