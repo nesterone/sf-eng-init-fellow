01406353dd7567256ecd2378eb6b1f23aecd5848
yzhbankov <yzhbankov2@gmail.com>
2015-12-24 10:54:32 +0200

    Update reverseArrayInPlace.js

--- 01-syntax/03-data-structures/reversing-an-array/reverseArrayInPlace.js
+++ 01-syntax/03-data-structures/reversing-an-array/reverseArrayInPlace.js
@@ -3,7 +3,15 @@
  *
  */
 
-// Your code here.
+var arrayValue = [1, 2, 3, 4, 5];
+function reverseArrayInPlace(x){
+var n = x.length - 1;
+for (var i = 0; i <= Math.floor((x.length - 1) / 2); i = i + 1){
+ r = x[i];
+ x[i] = x[n];
+ x[n] = r;
+ n = n - 1;}
+return x;}
 
 var arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
