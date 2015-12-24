89ea6aac196fd17934a28f9605099d8055f87db9
yzhbankov <yzhbankov2@gmail.com>
2015-12-24 12:05:10 +0200

    Update monthNames.js

--- 01-syntax/09-modules/month-names/monthNames.js
+++ 01-syntax/09-modules/month-names/monthNames.js
@@ -1,3 +1,14 @@
 /**
  * Created by nesterone on 11/1/15.
  */
+var month = function(){
+  var names = ["January","Febuary","March","Aprile","May","June",
+               "July","Augast","Sepetember","October","November","December"];
+  return {name: function(number){return names[number];},
+          number: function(name){return (names.indexOf(name))}};
+}();
+
+console.log(month.name(2));
+// → March
+console.log(month.number("November"));
+// → 10
