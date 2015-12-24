e65120f4ac95fa7868b08b88f9dfdd2edd4b44d5
yzhbankov <yzhbankov2@gmail.com>
2015-12-24 10:44:17 +0200

    Update range.js

--- 01-syntax/03-data-structures/the-sum-of-ranges/range.js
+++ 01-syntax/03-data-structures/the-sum-of-ranges/range.js
@@ -3,7 +3,23 @@
  *
  */
 
-// Your code here
+function range(starts,ends,step){
+  var k = 0, i = 0, b = [];
+  if (step > 0){
+    b[0] = starts;
+      while (b[i] < ends){
+    b[i+1] = b[i] + step;
+    i = i + 1;}
+  if (b[b.length-1] > ends)
+    b.splice(b.length - 1,1);}
+  if (step < 0){
+    b[0] = starts;
+    while (b[i] > ends){
+    b[i+1] = b[i] + step;
+    i = i + 1;}
+  if (b[b.length-1] != ends)
+    b.splice(b.length - 1,1);}
+ return b;}
 
 console.log(range(5, 2, -1));
 
