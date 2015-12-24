fa788df99fdd9fb5fbb3410da556560f2ff6de12
yzhbankov <yzhbankov2@gmail.com>
2015-12-24 11:55:51 +0200

    Update Sequence.js

---
+++ 01-syntax/05-objects/Sequence.js
@@ -0,0 +1,38 @@
+function logFive(seq){
+var c = 0;
+for (var i = seq.begin(); i < seq.end(); i++){
+console.log(seq.sequence[i]);
+ c++;
+if (c > 4) break;}}
+
+var object = [1,2,3];
+
+function ArraySeq(object){
+this.sequence = object;}
+
+ArraySeq.prototype.begin = function(){
+this.idx = 0;
+return this.idx}
+
+ArraySeq.prototype.end = function(){
+return this.sequence.length}
+
+RangeSeq.prototype = Object.create(ArraySeq.prototype);
+
+
+function RangeSeq(from,to){
+ this.sequence = [];
+  var c = from;
+ for (var i = 0; i < (to - from); i++){
+  c = from + i;
+  this.sequence.push(c);}}
+
+logFive(new ArraySeq([1, 2]));
+// → 1
+// → 2
+logFive(new RangeSeq(100, 1000));
+// → 100
+// → 101
+// → 102
+// → 103
+// → 104
