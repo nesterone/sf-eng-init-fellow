ff688638371e0a0c8cd70483f7f1d1267aa63c64
yzhbankov <yzhbankov2@gmail.com>
2015-12-24 11:43:50 +0200

    Update ageDifference.js

---
+++ 01-syntax/04-high-order-functions/ageDifference.js
@@ -0,0 +1,17 @@
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+var  age = 0, count = 0;
+for (var i = 0; i < ancestry.length; i++)
+  for (var j = 0; j < ancestry.length; j++)
+{if (ancestry[i].name == ancestry[j].mother){
+    count = count + 1;
+age = age + (ancestry[j].born - ancestry[i].born)}}
+console.log(ancestry);
+
+console.log(age/(count));
