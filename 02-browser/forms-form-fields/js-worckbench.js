115aeead8968e757b4b00a90d66e376fcc890d50
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-01-06 00:07:52 +0200

    the program was perfected and become shorter

--- 02-browser/forms-form-fields/js-worckbench.js
+++ 02-browser/forms-form-fields/js-worckbench.js
@@ -2,21 +2,19 @@
 <button id="button">Run</button>
 <pre id="output"></pre>
 
+
 <script>
   var res = '';
 var textarea = document.querySelector("textarea");
 var button = document.querySelector("button");
+var output = document.querySelector("#output");
 button.addEventListener('mousedown', function(){
   try{
    var plusOne = new Function(textarea.value);
-   newel(plusOne());}
+   output.innerText = plusOne();}
   catch(e){
-   newel('Name of error: ' + e.name);
-   newel('Error message: ' + e.message);}
+   output.innerText = 'Name of error: ' + e.name + '\n' + 
+     'Error message: ' + e.message;}
 });
- function newel(str){
-   var textelem = document.createElement('p');
-  var text = document.createTextNode(str);
-  textelem.appendChild(text);
-  document.body.appendChild(textelem);}
+ 
 </script>
