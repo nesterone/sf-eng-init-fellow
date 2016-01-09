2d69096a6c922d9c94787dd6ec77745e1aa5c317
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-01-10 00:56:31 +0200

    added A list programms

--- 01-syntax/03-data-structures/a-list/nth.js
+++ 01-syntax/03-data-structures/a-list/nth.js
@@ -4,7 +4,13 @@
  */
 
 
-//Your code here
+function nth(list, position) {
+    var i = 0;
+    for (var node = list; node; node = node.rest) {
+        i++;
+        if (position == i - 1) return node.value;
+    }
+}
 
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
