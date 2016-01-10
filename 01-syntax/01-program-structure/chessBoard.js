e8bb01025573115bf0bd06f768f254c6965cce07
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-01-10 22:27:36 +0200

    edit code style in program structure exercises

--- 01-syntax/01-program-structure/chessBoard.js
+++ 01-syntax/01-program-structure/chessBoard.js
@@ -1,9 +1,10 @@
-var n  = 8;
-for (var j = 1; j <= n; j++){
-    var b = '';
-    for (var i = 1; i <= n; i++){
+var size = 8;
+for (var j = 1; j <= size; j++) {
+    var row = '';
+    for (var i = 1; i <= size; i++) {
         if ((i + j) % 2 != 0)
-            b = b + '#';
-        else b = b + ' ';}
-    console.log(b);
+            row = row + '#';
+        else row = row + ' ';
+    }
+    console.log(row);
 }
