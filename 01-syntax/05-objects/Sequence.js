647919329b2394bb8f30a1afc461aee2e6a76d17
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-01-14 23:35:19 +0200

    was edited the code style and added some code to "StretchCell" program

--- 01-syntax/05-objects/Sequence.js
+++ 01-syntax/05-objects/Sequence.js
@@ -1,31 +1,38 @@
-function logFive(seq){
-var c = 0;
-for (var i = seq.begin(); i < seq.end(); i++){
-console.log(seq.sequence[i]);
- c++;
-if (c > 4) break;}}
+function logFive(seq) {
+    var counter = 0;
+    for (var i = seq.begin(); i < seq.end(); i++) {
+        console.log(seq.sequence[i]);
+        counter++;
+        if (counter > 4) break;
+    }
+}
 
-var object = [1,2,3];
+var object = [1, 2, 3];
 
-function ArraySeq(object){
-this.sequence = object;}
+function ArraySeq(object) {
+    this.sequence = object;
+}
 
-ArraySeq.prototype.begin = function(){
-this.idx = 0;
-return this.idx}
+ArraySeq.prototype.begin = function () {
+    this.idx = 0;
+    return this.idx
+}
 
-ArraySeq.prototype.end = function(){
-return this.sequence.length}
+ArraySeq.prototype.end = function () {
+    return this.sequence.length
+}
 
 RangeSeq.prototype = Object.create(ArraySeq.prototype);
 
 
-function RangeSeq(from,to){
- this.sequence = [];
-  var c = from;
- for (var i = 0; i < (to - from); i++){
-  c = from + i;
-  this.sequence.push(c);}}
+function RangeSeq(from, to) {
+    this.sequence = [];
+    var c = from;
+    for (var i = 0; i < (to - from); i++) {
+        c = from + i;
+        this.sequence.push(c);
+    }
+}
 
 logFive(new ArraySeq([1, 2]));
 // → 1
--- 01-syntax/05-objects/StretchCell.js
+++ 01-syntax/05-objects/StretchCell.js
@@ -1,27 +1,61 @@
-StretchCell = function(inner, width, height){
-  this.inner = inner;
-  var variable = this.inner.text;
-  if (variable.length < height){
-    while (height - variable.lenght > 0){
-      variable = variable.concat([""]);
-      height = height - 1;}}
-  
-  this.height = variable.length;
-  
-  this.inner.text = variable.map(function(i){
-  if (i.length < width)    
-    return i.concat(repeat("_",width - i.length));
-    else
-      return i;});}
+function repeat(string, times) {
+    var result = "";
+    for (var i = 0; i < times; i++)
+        result += string;
+    return result;
+}
 
-StretchCell.prototype.minWidth = function(){
-  return this.inner.minWidth()};
+function TextCell(text) {
+    this.text = text.split("\n");
+}
+TextCell.prototype.minWidth = function () {
+    return this.text.reduce(function (width, line) {
+        return Math.max(width, line.length);
+    }, 0);
+};
+TextCell.prototype.minHeight = function () {
+    return this.text.length;
+};
+TextCell.prototype.draw = function (width, height) {
+    var result = [];
+    for (var i = 0; i < height; i++) {
+        var line = this.text[i] || "";
+        result.push(line + repeat(" ", width - line.length));
+    }
+    return result;
+};
 
-StretchCell.prototype.minHeight = function(){
-  return this.inner.minHeight()};
+StretchCell = function (inner, width, height) {
+    this.inner = inner;
+    var variable = this.inner.text;
+    if (variable.length < height) {
+        while (height - variable.lenght > 0) {
+            variable = variable.concat([""]);
+            height = height - 1;
+        }
+    }
 
-StretchCell.prototype.draw = function(w,h){
-  return this.inner.draw(w,h)};
+    this.height = variable.length;
+
+    this.inner.text = variable.map(function (i) {
+        if (i.length < width)
+            return i.concat(repeat("_", width - i.length));
+        else
+            return i;
+    });
+}
+
+StretchCell.prototype.minWidth = function () {
+    return this.inner.minWidth()
+};
+
+StretchCell.prototype.minHeight = function () {
+    return this.inner.minHeight()
+};
+
+StretchCell.prototype.draw = function (w, h) {
+    return this.inner.draw(w, h)
+};
 
 var sc = new StretchCell(new TextCell("abc"), 1, 2);
 console.log(sc.minWidth());
