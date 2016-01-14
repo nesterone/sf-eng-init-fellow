87e611d654fbfa0485a30ad899eaa816caf3431c
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-01-14 23:39:18 +0200

    was edited the code style and added some code to leckedBox.js

--- 01-syntax/07-error/lockedBox.js
+++ 01-syntax/07-error/lockedBox.js
@@ -1,20 +1,36 @@
+var box = {
+    locked: true,
+    unlock: function() { this.locked = false; },
+    lock: function() { this.locked = true;  },
+    _content: [],
+    get content() {
+        if (this.locked) throw new Error("Locked!");
+        return this._content;
+    }
+};
+
 function withBoxUnlocked(body) {
-  box.unlock();
-  try { body() }
-  catch(e){ if(!box.locked)
-    console.log(e);}
-  finally{
-    box.lock()}
+    box.unlock();
+    try {
+        body()
+    }
+    catch (e) {
+        if (!box.locked)
+            console.log(e);
+    }
+    finally {
+        box.lock()
+    }
 }
-withBoxUnlocked(function() {
-  box.content.push("gold piece");
+withBoxUnlocked(function () {
+    box.content.push("gold piece");
 });
 try {
-  withBoxUnlocked(function() {
-    throw new Error("Pirates on the horizon! Abort!");
-  });
+    withBoxUnlocked(function () {
+        throw new Error("Pirates on the horizon! Abort!");
+    });
 } catch (e) {
-  console.log("Error raised:", e);
+    console.log("Error raised:", e);
 }
 console.log(box.locked);
 
