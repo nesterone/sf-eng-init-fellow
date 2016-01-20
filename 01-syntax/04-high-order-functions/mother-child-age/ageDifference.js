981b79b615df59bf52ff776fb97b28536464ac18
nesterone <igornester@gmail.com>
2016-01-20 09:25:09 +0200

    - provide data for high-order functions excersices

---
+++ 01-syntax/04-high-order-functions/mother-child-age/ageDifference.js
@@ -0,0 +1,14 @@
+function average(array) {
+    function plus(a, b) { return a + b; }
+    return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+    byName[person.name] = person;
+});
+
+// Your code here.
+
+
+// → 31.2
