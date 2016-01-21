dcc6d39543e08fd0bc90ae39bfdca26a8e6f2fa4
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-01-21 22:18:40 +0200

    solved problem with circular dependency for commonjs approach

--- 01-syntax/09-modules/circular-deps/modules.js
+++ 01-syntax/09-modules/circular-deps/modules.js
@@ -1,12 +1,13 @@
 function require(name) {
-    if (name in require.cache)
+      if (name in require.cache)
         return require.cache[name];
 
     var code = new Function("exports, module", readFile(name));
     var exports = {}, module = {exports: exports};
+    require.cache[name] = module.exports;
     code(exports, module);
 
-    require.cache[name] = module.exports;
+
     return module.exports;
 }
 require.cache = Object.create(null);
