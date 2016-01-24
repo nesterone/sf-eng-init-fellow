b6b755584e7a31a180b2d554bb58327009646968
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-01-24 13:27:07 +0200

    edited deep equal to old version

--- 01-syntax/03-data-structures/deep-comparation/deepEqual.js
+++ 01-syntax/03-data-structures/deep-comparation/deepEqual.js
@@ -8,14 +8,11 @@ function deepEqual(object_1, object_2) {
     var counter_1 = 0;
     var counter_2 = 0;
 
-    if (object_1 === object_2)
-        return true;
-
-    if (typeof object_1 != 'object' || object_1 == null ||
-        typeof object_2 != 'object' || object_2 == null)
+    if ((typeof object_1 != 'object' && object_1 == null) &&
+        (typeof object_2 != 'object' && object_2 == null))
         return false;
 
-
+    if (object_1 === object_2) return true;
 
     for (var node in object_1)
         counter_1++;
@@ -26,7 +23,7 @@ function deepEqual(object_1, object_2) {
             return false;
     }
 
-    return counter_1 == counter_2;
+    return (counter_1 == counter_2);
 
 }
 
@@ -37,19 +34,3 @@ console.log(deepEqual(obj, {here: 1, object: 2}));
 // → false
 console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 // → true
-
-
-
-console.log(deepEqual({a: {t: {msg: "hello"}, v: "sssss"}},
-    {here: {is: "an"}, object: 2}));
-//→ false
-
-console.log(deepEqual({a: {v: "sssss", t: {msg: "hello"}}},
-    {a: {t: {msg: "hell"}, v: "sssss"}}));
-
-//→ false
-
-console.log(deepEqual({a: {t: {msg: "hello"}, v: "sssss"}},
-    {a: {t: {msg: "hello"}, v: "sssss"}}));
-
-//→ true
