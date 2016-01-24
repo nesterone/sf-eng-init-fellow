4f539878874f6c5e4f4e115bf1c8436efe9be616
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-01-24 13:29:27 +0200

    edited circular dependency to old version

--- 01-syntax/09-modules/circular-deps/modules.js
+++ 01-syntax/09-modules/circular-deps/modules.js
@@ -1,13 +1,12 @@
 function require(name) {
-      if (name in require.cache)
+    if (name in require.cache)
         return require.cache[name];
 
     var code = new Function("exports, module", readFile(name));
     var exports = {}, module = {exports: exports};
-    require.cache[name] = module.exports;
     code(exports, module);
 
-
+    require.cache[name] = module.exports;
     return module.exports;
 }
 require.cache = Object.create(null);
@@ -70,4 +69,5 @@ function readFile(name){
     xhr.send(null);
     return xhr.responseText;
 }
+
 
