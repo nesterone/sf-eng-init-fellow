64fee662aeba99f41c7c3998c5a36ec9630dfce1
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-01-24 13:45:59 +0200

    edited style code in deep equal

--- 01-syntax/03-data-structures/deep-comparation/deepEqual.js
+++ 01-syntax/03-data-structures/deep-comparation/deepEqual.js
@@ -8,22 +8,22 @@ function deepEqual(object_1, object_2) {
     var counter_1 = 0;
     var counter_2 = 0;
 
-    if (object_1 === object_2)
+    if (object_1 === object_2) {
         return true;
-
+    }
     if (typeof object_1 != 'object' || object_1 == null ||
-        typeof object_2 != 'object' || object_2 == null)
+        typeof object_2 != 'object' || object_2 == null) {
         return false;
+    }
 
-
-
-    for (var node in object_1)
-        counter_1++;
+    for (var node in object_1) counter_1++;
 
     for (var node in object_2) {
         counter_2++;
-        if (!(node in object_1) || !deepEqual(object_1[node], object_2[node]))
+        if (!(node in object_1) || !deepEqual(object_1[node], object_2[node])) {
+
             return false;
+        }
     }
 
     return counter_1 == counter_2;
@@ -31,6 +31,7 @@ function deepEqual(object_1, object_2) {
 }
 
 var obj = {here: {is: "an"}, object: 2};
+
 console.log(deepEqual(obj, obj));
 // → true
 console.log(deepEqual(obj, {here: 1, object: 2}));
@@ -39,16 +40,54 @@ console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 // → true
 
 
-console.log(deepEqual({a: {t: {msg: "hello"}, v: "sssss"}},
-    {here: {is: "an"}, object: 2}));
+console.log(deepEqual({
+        a: {
+            t: {
+                msg: "hello"
+            },
+            v: "sssss"
+        }
+    },
+    {
+        here: {
+            is: "an"
+        },
+        object: 2
+    }));
 //→ false
 
-console.log(deepEqual({a: {v: "sssss", t: {msg: "hello"}}},
-    {a: {t: {msg: "hell"}, v: "sssss"}}));
+console.log(deepEqual({
+    a: {
+        v: "sssss",
+        t: {
+            msg: "hello"
+        }
+    }
+}, {
+    a: {
+        t: {
+            msg: "hell"
+        },
+        v: "sssss"
+    }
+}));
 
 //→ false
 
-console.log(deepEqual({a: {t: {msg: "hello"}, v: "sssss"}},
-    {a: {t: {msg: "hello"}, v: "sssss"}}));
+console.log(deepEqual({
+    a: {
+        t: {
+            msg: "hello"
+        },
+        v: "sssss"
+    }
+}, {
+    a: {
+        t: {
+            msg: "hello"
+        },
+        v: "sssss"
+    }
+}));
 
 //→ true
