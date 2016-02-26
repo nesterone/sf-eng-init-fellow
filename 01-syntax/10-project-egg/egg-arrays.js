d1bb05ff267d60f76c78b49b0a98ff615c706c72
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-02-26 22:57:23 +0200

    move project-egg tests to html file

--- 01-syntax/10-project-egg/egg-arrays.js
+++ 01-syntax/10-project-egg/egg-arrays.js
@@ -17,12 +17,3 @@ topEnv["length"] = function (array) {
 topEnv["element"] = function (array, n) {
     return array[n];
 };
-
-run("do(define(sum, fun(array,",
-    "     do(define(i, 0),",
-    "        define(sum, 0),",
-    "        while(<(i, length(array)),",
-    "          do(define(sum, +(sum, element(array, i))),",
-    "             define(i, +(i, 1)))),",
-    "        sum))),",
-    "   print(sum(array(1, 2, 3))))");
--- 01-syntax/10-project-egg/egg-comments.js
+++ 01-syntax/10-project-egg/egg-comments.js
@@ -7,13 +7,3 @@ function skipSpace(string) {
     if (first == -1) return "";
     return string.slice(first[0].length);
 }
-
-console.log(parse("# hello\nx"));
-// ? {type: "word", name: "x"}
-
-console.log(parse("a # one\n   # two\n()"));
-// ? {type: "apply",
-//    operator: {type: "word", name: "a"},
-//    args: []}
-
-//console.log(Object.getPrototypeOf());
