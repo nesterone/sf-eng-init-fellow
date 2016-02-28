7ea89b56bc62724a24947fcdaa0a526e2c6dcc3f
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-02-28 21:52:06 +0200

    added bouncing ball solution

--- 02-browser/03-drawing-on-canvas/03-a-bouncing-ball/bouncingBall.js
+++ 02-browser/03-drawing-on-canvas/03-a-bouncing-ball/bouncingBall.js
@@ -1,17 +1,44 @@
 var cx = document.querySelector("canvas").getContext("2d");
-
+var ballPosX = 100, ballPosY = 320;
+var horizDirection = 1, vertDirection = 1;
 var lastTime = null;
-
 function frame(time) {
-    if (lastTime != null){
+    if (lastTime != null)
         updateAnimation(Math.min(100, time - lastTime) / 1000);
-    }
     lastTime = time;
     requestAnimationFrame(frame);
 }
 
 requestAnimationFrame(frame);
 
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Vector.prototype.times = function (factor) {
+    return new Vector(this.x * factor, this.y * factor);
+};
+
 function updateAnimation(step) {
-    // Your code here.
+
+    var x0 = 20, y0 = 20, rectWidth = 350, rectHeigh = 350;
+    var x = x0 + 40 + new Vector(step, step * 0.3).times(ballPosX * 100).x;
+    var y = y0 + 40 + new Vector(step, step * 0.3).times(ballPosY * 100).y;
+
+    if (x + 11 >= rectWidth + x0 || x - 9 <= x0) {
+        horizDirection = -1 * horizDirection;
+    }
+    if (y + 11 >= rectHeigh + y0 || y - 9 <= y0) {
+        vertDirection = -1 * vertDirection;
+    }
+    ballPosX = ballPosX + horizDirection * 1;
+    ballPosY = ballPosY + vertDirection * 1;
+    cx.clearRect(0, 0, 400, 400);
+    cx.save();
+    cx.strokeRect(x0, y0, rectWidth, rectHeigh);
+    cx.beginPath();
+    cx.arc(x + 1, y + 1, 10, 0, 7);
+    cx.fillStyle = 'blue';
+    cx.fill();
+    cx.restore();
 }
