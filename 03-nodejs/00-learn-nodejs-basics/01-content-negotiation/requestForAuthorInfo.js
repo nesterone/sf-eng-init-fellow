5b904a1dd488a668bfabb1821966151a1a4a3d28
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-03-21 20:59:23 +0200

    done first exercise content negotiation create and treat request to eloquent server

--- 03-nodejs/00-learn-nodejs-basics/01-content-negotiation/requestForAuthorInfo.js
+++ 03-nodejs/00-learn-nodejs-basics/01-content-negotiation/requestForAuthorInfo.js
@@ -1,1 +1,24 @@
 // Your code here
+var http = require("http");
+var reqTypes = ["text/plain", "text/html", "application/json"];
+
+for (var i = 0; i < reqTypes.length; i++) {
+    var request = http.request({
+        hostname: "eloquentjavascript.net",
+        path: "/author",
+        method: "GET",
+        headers: {
+            Accept: reqTypes[i]
+        }
+    }, function (response) {
+        console.log("Status code: ", response.statusCode);
+        response.on("data", function (chunk) {
+            console.log(chunk.toString());
+        });
+        response.on("end", function () {
+            return;
+        });
+    });
+
+    request.end();
+}
