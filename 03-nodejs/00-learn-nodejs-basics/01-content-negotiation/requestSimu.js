dad58fcfaf56f27b4afa226999b4576697b63389
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-03-21 22:06:30 +0200

    add server and request simulator

---
+++ 03-nodejs/00-learn-nodejs-basics/01-content-negotiation/requestSimu.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Iaroslav Zhbankov on 21.03.2016.
+ */
+var http = require("http");
+
+var request = http.request({
+    hostname: "localhost",
+    port: 8000,
+    path: "/text.txt",
+    method: "DELETE"
+}, function(response) {
+    console.log(response.statusCode);
+    response.on("end", function() {
+        return;
+    });
+});
+request.end();
