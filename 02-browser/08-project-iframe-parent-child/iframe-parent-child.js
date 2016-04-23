3ef90537d85eddde0efb330fabcfcadf96f4784b
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-04-23 23:10:59 +0300

    send message from child to parent

--- 02-browser/08-project-iframe-parent-child/iframe-parent-child.js
+++ 02-browser/08-project-iframe-parent-child/iframe-parent-child.js
@@ -1,3 +1,9 @@
 /**
  * Created by Iaroslav Zhbankov on 21.04.2016.
  */
+$.receiveMessage(
+    function(e){
+        alert( e.data );
+    },
+    'http://localhost:8000/js-eng-init-clone/02-browser/08-project-iframe-parent-child/index2.html'
+);
