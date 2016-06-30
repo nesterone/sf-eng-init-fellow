e1ea4e86207c27a1665ffa45840c188976d4a91e
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-06-30 22:22:36 +0300

    complete exercise

---
+++ 05-spa-development/01-basic-backbone/js/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Iaroslav Zhbankov on 30.06.2016.
+ */
+var app = app || {};
+var ENTER_KEY = 13;
+
+$(function() {
+
+    // Kick things off by creating the **App**.
+    new app.AppView();
+
+});
