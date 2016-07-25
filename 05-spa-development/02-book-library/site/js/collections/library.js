60b22a264d67b6162a5fe5404ed6a433354f3564
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-07-25 22:01:59 +0300

    fill the skiletone files

--- 05-spa-development/02-book-library/site/js/collections/library.js
+++ 05-spa-development/02-book-library/site/js/collections/library.js
@@ -1,3 +1,8 @@
 /**
  * Created by Iaroslav Zhbankov on 25.07.2016.
  */
+var app = app || {};
+
+app.Library = Backbone.Collection.extend({
+    model: app.Book
+});
--- 05-spa-development/02-book-library/site/js/views/library.js
+++ 05-spa-development/02-book-library/site/js/views/library.js
@@ -1,3 +1,29 @@
 /**
  * Created by Iaroslav Zhbankov on 25.07.2016.
  */
+var app = app || {};
+
+app.LibraryView = Backbone.View.extend({
+    el: '#books',
+
+    initialize: function( initialBooks ) {
+        this.collection = new app.Library( initialBooks );
+        this.render();
+    },
+
+    // render library by rendering each book in its collection
+    render: function() {
+        this.collection.each(function( item ) {
+            this.renderBook( item );
+        }, this );
+    },
+
+    // render a book by creating a BookView and appending the
+    // element it renders to the library's element
+    renderBook: function( item ) {
+        var bookView = new app.BookView({
+            model: item
+        });
+        this.$el.append( bookView.render().el );
+    }
+});
