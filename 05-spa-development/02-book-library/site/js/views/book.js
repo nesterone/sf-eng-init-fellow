765c347db81f3dabe2168fe9691cde50c5ce736c
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-07-25 22:14:22 +0300

    add functionality

--- 05-spa-development/02-book-library/site/js/views/book.js
+++ 05-spa-development/02-book-library/site/js/views/book.js
@@ -4,6 +4,17 @@
 var app = app || {};
 
 app.BookView = Backbone.View.extend({
+    events: {
+        'click .delete': 'deleteBook'
+    },
+
+    deleteBook: function() {
+        //Delete model
+        this.model.destroy();
+
+        //Delete view
+        this.remove();
+    },
     tagName: 'div',
     className: 'bookContainer',
     template: _.template( $( '#bookTemplate' ).html() ),
